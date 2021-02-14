72ed98fba7eb3dc86d7b337cf074ee8ec3868907
PureGero <puregero@gmail.com>
2021-02-14 17:33:28 +1000

    Search around the chunk and into the nether/overworld for other locks when locking chunks

--- MultiPaper-Master/src/main/java/puregero/multipaper/server/handlers/LockChunkHandler.java
+++ MultiPaper-Master/src/main/java/puregero/multipaper/server/handlers/LockChunkHandler.java
@@ -8,13 +8,53 @@ import java.io.DataInputStream;
 import java.io.IOException;
 
 public class LockChunkHandler implements Handler {
+    private static final int CHUNK_SEARCH_RADIUS = 3;
+    
     @Override
     public void handle(ServerConnection connection, DataInputStream in, DataOutputSender out) throws IOException {
+        String locker;
+        
         String world = in.readUTF();
         int cx = in.readInt();
         int cz = in.readInt();
 
-        String locker = ChunkLock.lock(connection.getBungeeCordName(), world, cx, cz);
+        // Check if any chunks around this chunk are locked
+        for (int x = -CHUNK_SEARCH_RADIUS; x <= CHUNK_SEARCH_RADIUS; x++) {
+            for (int z = -CHUNK_SEARCH_RADIUS; z <= CHUNK_SEARCH_RADIUS; z++) {
+                locker = ChunkLock.getLockHolder(world, cx + x, cz + z);
+                if (locker != null && !locker.equals(connection.getBungeeCordName()) && ServerConnection.isAlive(locker)) {
+                    out.writeUTF("lockedChunk");
+                    out.writeUTF(locker);
+                    out.send();
+                    return;
+                }
+            }
+        }
+        
+        if (world.contains("_the_end")) {
+            // Do nothing for the end
+        } else if (world.contains("_nether")) {
+            // Check if overworld equiv chunk is locked
+            String worldName = world.split("_")[0];
+            locker = ChunkLock.getLockHolder(worldName, cx * 8, cz * 8);
+            if (locker != null && !locker.equals(connection.getBungeeCordName()) && ServerConnection.isAlive(locker)) {
+                out.writeUTF("lockedChunk");
+                out.writeUTF(locker);
+                out.send();
+                return;
+            }
+        } else {
+            // Check if nether equiv chunk is locked
+            locker = ChunkLock.getLockHolder(world + "_nether", cx / 8, cz / 8);
+            if (locker != null && !locker.equals(connection.getBungeeCordName()) && ServerConnection.isAlive(locker)) {
+                out.writeUTF("lockedChunk");
+                out.writeUTF(locker);
+                out.send();
+                return;
+            }
+        }
+        
+        locker = ChunkLock.lock(connection.getBungeeCordName(), world, cx, cz);
 
         if (locker != null && !ServerConnection.isAlive(locker)) {
             // Ignore dead lockers

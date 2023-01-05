221adc353be1ff98c504cd1e1dafc681654a553a
mohammed jasem alaajel <xrambad@gmail.com>
2023-01-06 00:46:09 +0400

    update paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.papermc.paperweight.patcher") version "1.3.11"
+    id("io.papermc.paperweight.patcher") version "1.4.0"
 }
 
 repositories {

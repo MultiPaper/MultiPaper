e8d60e90c633b2f54a8ead8a846f35b49c58efe6
Kugge <sofiane.djerbi38@gmail.com>
2023-02-02 02:14:43 +0100

    Set Purpur as upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven("https://repo.papermc.io/repository/maven-public/") {
         content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
@@ -45,8 +45,8 @@ subprojects {
 
     repositories {
         mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
@@ -58,22 +58,27 @@ subprojects {
 paperweight {
     serverProject.set(project(":MultiPaper-Server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set("https://repo.papermc.io/repository/maven-public/")
+    decompileRepo.set("https://repo.papermc.io/repository/maven-public/")
 
-    usePaperUpstream(providers.gradleProperty("paperRef")) {
-        withPaperPatcher {
+    useStandardUpstream("Purpur") {
+        url.set(github("PurpurMC", "Purpur"))
+        ref.set(providers.gradleProperty("purpurRef"))
+        
+        withStandardPatcher {
+            apiSourceDirPath.set("Purpur-API") 
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("MultiPaper-API"))
 
+            serverSourceDirPath.set("Purpur-Server")
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("MultiPaper-Server"))
         }
     }
-    
-    tasks.register("paperRefLatest") {
+
+    tasks.register("purpurRefLatest") {
         // Update the paperRef in gradle.properties to be the latest commit
-        val tempDir = layout.cacheDir("paperRefLatest");
+        val tempDir = layout.cacheDir("purpurRefLatest");
         val file = "gradle.properties";
         
         doFirst {
@@ -81,15 +86,15 @@ paperweight {
                     val sha: String
             )
 
-            val paperLatestCommitJson = layout.cache.resolve("paperLatestCommit.json");
-            download.get().download("https://api.github.com/repos/PaperMC/Paper/commits/master", paperLatestCommitJson);
-            val paperLatestCommit = gson.fromJson<paper.libs.com.google.gson.JsonObject>(paperLatestCommitJson)["sha"].asString;
+            val purpurLatestCommitJson = layout.cache.resolve("purpurLatestCommit.json");
+            download.get().download("https://api.github.com/repos/PurpurMC/Purpur/commits/ver/1.19.3", purpurLatestCommitJson);
+            val purpurLatestCommit = gson.fromJson<paper.libs.com.google.gson.JsonObject>(purpurLatestCommitJson)["sha"].asString;
 
             copy {
                 from(file)
                 into(tempDir)
                 filter { line: String ->
-                    line.replace("paperRef = .*".toRegex(), "paperRef = $paperLatestCommit")
+                    line.replace("purpurRef = .*".toRegex(), "purpurRef = $purpurLatestCommit")
                 }
             }
         }

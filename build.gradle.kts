5fd072728b4dca57f6cc5ee5edb5b079f1eceba8
PureGero <puregero@gmail.com>
2023-03-27 16:09:07 +0900

    Update upstream (1.19.4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.0"
-    id("io.papermc.paperweight.patcher") version "1.5.2"
+    id("io.papermc.paperweight.patcher") version "1.5.3"
 }
 
 repositories {
@@ -17,8 +17,8 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
-    paperclip("io.papermc:paperclip:3.0.2")
+    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    paperclip("io.papermc:paperclip:3.0.3")
 }
 
 allprojects {
